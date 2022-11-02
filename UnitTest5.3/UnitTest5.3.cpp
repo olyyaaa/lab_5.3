d032c29414cff087d39b77f049356a1d52d2f45c
DESKTOP-B2ESKHB\olhat <olhatytysh@gmail.com>
2022-11-02 23:09:18 +0200

    third test

---
+++ UnitTest5.3/UnitTest5.3.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest53
+{
+	TEST_CLASS(UnitTest53)
+	{
+	public:
+		
+		TEST_METHOD(TestzFunction)
+		{
+			const double x = 2;
+			double S = 0;
+			double j = 2;
+			double k = 4;
+			double& res = temp;
+			double expected = 2338;
+			z(j, k, res);
+
+			Assert::AreEqual(res, expected);
+
+
+		}
+		
+		}
+	};
+}
